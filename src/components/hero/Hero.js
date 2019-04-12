4fd27f9d35e3165c7f4e8c5c9bf57452ba7a3ba5
Casey Heinemeyer <casey@Caseys-MacBook-Pro.local>
2019-04-12 15:31:51 -0700

    css

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
 
     <div className="hero">
       <div className="hero__heading">
-        <h1 className="hero__text--main">JD's Shoppe</h1>
+        <h1 className="hero__text--main">JD's shoppe</h1>
         <h3 className="hero__text--sub">We have what you want</h3>
       </div>
       <HeroLinks />
